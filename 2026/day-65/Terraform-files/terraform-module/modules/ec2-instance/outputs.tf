71d23617220153247424a32374b2e8f596ab563f
AKASH PRAKASH AHIR <akashahir50@gmail.com>
2026-08-11 11:36:39 +0530

    Update outputs.tf

--- 2026/day-65/Terraform-files/terraform-module/modules/ec2-instance/outputs.tf
+++ 2026/day-65/Terraform-files/terraform-module/modules/ec2-instance/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.my_aws_instance.id
+  value = aws_instance.my_aws_instance.id
 }
 
 output "public_ip" {
   description = "Public IP address of instance"
-  value       = aws_instance.my_aws_instance.public_ip
+  value = aws_instance.my_aws_instance.public_ip
 }
 
 output "private_ip" {
   description = "Private IP address of instance"
-  value       = aws_instance.my_aws_instance.private_ip
+  value = aws_instance.my_aws_instance.private_ip
 }
