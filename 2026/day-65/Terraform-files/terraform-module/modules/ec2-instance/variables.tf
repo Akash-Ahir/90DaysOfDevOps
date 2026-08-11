e1dec8ad3c1cb65c3a14887b436dc61512f24799
AKASH PRAKASH AHIR <akashahir50@gmail.com>
2026-08-11 11:37:13 +0530

    Update variables.tf

--- 2026/day-65/Terraform-files/terraform-module/modules/ec2-instance/variables.tf
+++ 2026/day-65/Terraform-files/terraform-module/modules/ec2-instance/variables.tf
@@ -1,31 +1,31 @@
 variable "ami_id" {
   description = "ami id of EC2 instance"
-  type        = string
+  type = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
+  type = string
+  default = "t3.micro"
 }
 
 variable "subnet_id" {
   description = "Subnet for EC2 instance "
-  type        = string
+  type = string
 }
 
 variable "security_group_ids" {
   description = "Security groups for EC2 instance"
-  type        = list(string)
+  type = list(string)
 }
 
 variable "instance_name" {
   description = "Name of EC2 instance"
-  type        = string
+  type = string
 }
 
 variable "tags" {
   description = "Additional tags of EC2 instance"
-  type        = map(string)
-  default     = {}
+  type = map(string)
+  default = {}
 }
