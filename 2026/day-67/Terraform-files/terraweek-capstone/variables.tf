a0d5d8b4b9b503266b66f7ad4d7f2b44e4ddce1c
AKASH PRAKASH AHIR <akashahir50@gmail.com>
2026-08-17 09:14:30 +0530

    Create variables.tf

---
+++ 2026/day-67/Terraform-files/terraweek-capstone/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type = string
+  default = "terraweek"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ingress_ports" {
+  type = list(number)
+  default = [22, 80]
+}
